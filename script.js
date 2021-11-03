3c3a009442dd374267b4b0c43f95d55dac743450
Neo Gullberg <neogullberg04@gmail.com>
2021-11-03 20:23:44 +0100

    monke commit

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-const navSlide = () => {
-	const burgerBtn = document.querySelector(".burger-btn");
-	const navMenu = document.querySelector(".nav-menu");
-	const navLinks = document.querySelectorAll(".nav-menu li");
+// mobile hamburger menu
+const burgerBtn = document.querySelector(".burger-btn");
+const navMenu = document.querySelector(".nav-menu");
 
-	burgerBtn.addEventListener("click", () => {
-		// Toggle burgerBtn
-		burgerBtn.classList.toggle("active")
+burgerBtn.addEventListener("click", navSlide)
 
-		// Toggle navMenu
-		navMenu.classList.toggle("active");
-
-		// Animate navLinks
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = "";
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-			}
-		});
-	});
+function navSlide() {
+	burgerBtn.classList.toggle("active");
+	navMenu.classList.toggle("active");
 }
 
-navSlide();
+// image showcase
+let previewImage = document.querySelectorAll(".preview-image")
+
+if(previewImage) {
+    previewImage.forEach(function(image, index) {
+        image.onclick = function() {
+            let getImageCSS = window.getComputedStyle(image); // gets the css-styling of the image
+            let getImageUrl = getImageCSS.getPropertyValue("background-image"); // gets specifically the background-image property
+            let splitImageUrl = getImageUrl.split("/img/"); // splits the image url into two pieces, index 0 and 1
+            let setNewImageUrl = splitImageUrl[1].replace('")', ''); // sets new image url by replacing a part (index 1, second half) of the already split image url
+
+            let lightbox = document.body;
+            let lightboxBackground = document.createElement("div");
+            lightbox.appendChild(lightboxBackground);
+            lightboxBackground.setAttribute("class", "lightbox-background");
+            lightboxBackground.setAttribute("onclick", "closeImage()");
+
+            let lightboxImage = document.createElement("img");
+            lightboxBackground.appendChild(lightboxImage);
+            lightboxImage.setAttribute("src", "img/" + setNewImageUrl);
+            lightboxImage.setAttribute("id", "current-image")
+        }
+    });
+}
+
+function closeImage() {
+    document.querySelector(".lightbox-background").remove();
+}
